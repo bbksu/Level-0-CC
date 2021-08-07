f88939a05b45f620d0cfaad9d8e558cfab489b9c
bbksu <87952662+bbksu@users.noreply.github.com>
2021-08-08 00:34:22 +0200

    Added task0-5.py

---
+++ task0-5.py
@@ -0,0 +1,10 @@
+import math
+
+
+def area_of_triangle(side_a=15, side_b=20, side_c=17):
+    semi_perimeter = (side_a + side_b + side_c) / 2
+    heron = semi_perimeter * ((semi_perimeter - side_a) * (semi_perimeter - side_b) * (semi_perimeter - side_c))
+    heron = math.sqrt(heron)
+    Area = heron
+    return Area
+
