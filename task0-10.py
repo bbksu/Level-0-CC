c97e45f55aa39b9e8d5d49454793f688f0debcb7
bbksu <87952662+bbksu@users.noreply.github.com>
2021-08-08 01:41:34 +0200

    fixed output formatting

--- task0-10.py
+++ task0-10.py
@@ -1,9 +1,8 @@
 def common_letters(string_a="house", string_b="computers"):
     letters = ""
     set_list = list(set(string_a.lower()) & set(string_b.lower()))
-    print("Common letters:")
     for letter in set_list:
         letters += letter + ", "
 
     letters = letters.rstrip(", ")
-    print(letters)
+    print("Common letters:", letters)
