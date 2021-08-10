7a8f2bd04612818f056d16905eee859aa561b9f3
bbksu <87952662+bbksu@users.noreply.github.com>
2021-08-10 10:58:00 +0200

    Update task0-8.py

--- task0-8.py
+++ task0-8.py
@@ -1,5 +1,7 @@
 def hour_plus_minute(hours=71):
     hour = int(hours / 60)
     minute = (hours / 60) - hour
-    minute = int(minute * 60)
-    print(f"{hours} will become \"{hour} hours, {minute} minutes\"")
+    minute = int((minute * 60) + 0.1)
+    plural_h = "hour" if hour == 1 else "hours"
+    plural_m = "minute" if minute == 1 else "minutes"
+    print(f"{hours} will become \"{hour} {plural_h}, {minute} {plural_m}\"")
